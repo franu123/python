3e6fe7d752710585d1df006468d695667cd4a24c
franu <franu625@gmail.com>
2024-10-30 20:28:57 +0530

    yess

---
+++ dsaa.py
@@ -0,0 +1,14 @@
+# Take input from the user
+Number1=2
+Number2=4
+sum=Number1+Number2
+print(sum)
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the two numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", sum)
